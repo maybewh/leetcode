56c220b86b955d124e1a401dc1663b65b4404930
maybewh <122131004@qq.com>
2021-06-15 23:46:09 +0800

    任务调度

---
+++ src/com/leetcode/top100/TaskScheduler_621.java
@@ -0,0 +1,22 @@
+package com.leetcode.top100;
+
+/**
+ * todo
+ * 桶思想
+ */
+public class TaskScheduler_621 {
+
+    public int leastInterval(char[] tasks, int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+        TaskScheduler_621 taskScheduler = new TaskScheduler_621();
+        char[] tasks = {'A','A','A','B','B','B'};
+        int n = 2;
+        int result = taskScheduler.leastInterval(tasks, n);
+        System.out.println(result);
+    }
+
+}
