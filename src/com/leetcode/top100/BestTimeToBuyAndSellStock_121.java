683a3b43bacf9fdd14022b35715137fe55823a2c
maybewh <122131004@qq.com>
2021-06-16 22:35:38 +0800

    买卖股票的最佳时机

---
+++ src/com/leetcode/top100/BestTimeToBuyAndSellStock_121.java
@@ -0,0 +1,56 @@
+package com.leetcode.top100;
+
+public class BestTimeToBuyAndSellStock_121 {
+
+    /**
+     * 自我的解答，
+     * 4ms 28.14%
+     * 55.1 MB 5.05%
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+
+        int max = 0;
+        int[] mins = new int[prices.length]; // 存储到当前节点最小的值
+        mins[0] = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - mins[i - 1] > max) {
+                max = prices[i] - mins[i - 1];
+            }
+            if (prices[i] < mins[i - 1]) {
+                mins[i] = prices[i];
+            } else {
+                mins[i] = mins[i - 1];
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * 优化，当为O(n)时，根本不需要数组维护
+     * @param prices
+     * @return
+     */
+    public int maxResult(int[] prices) {
+
+        int maxProfit = 0;
+        int minPrices= Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrices) {
+                minPrices = prices[i];
+            } else if (prices[i] - minPrices > maxProfit) {
+                maxProfit = prices[i] - minPrices;
+            }
+        }
+        return maxProfit;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/leetcode/top100/TaskScheduler_621.java
+++ src/com/leetcode/top100/TaskScheduler_621.java
@@ -8,13 +8,30 @@ public class TaskScheduler_621 {
 
     public int leastInterval(char[] tasks, int n) {
 
+        int[] count = new int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            int index = tasks[i] - 'A';
+            count[index]++;
+        }
+
+        int maxCount = 0;
+        int maxValue = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxValue) {
+                maxValue = count[i];
+                maxCount = 1;
+            } else if (count[i] == maxValue) {
+                maxCount++;
+            }
+        }
+        return Math.max(tasks.length, maxCount + (n + 1) * (maxValue - 1));
     }
 
     public static void main(String[] args) {
 
         TaskScheduler_621 taskScheduler = new TaskScheduler_621();
         char[] tasks = {'A','A','A','B','B','B'};
-        int n = 2;
+        int n = 0;
         int result = taskScheduler.leastInterval(tasks, n);
         System.out.println(result);
     }
