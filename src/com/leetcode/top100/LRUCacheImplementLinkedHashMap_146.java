d22b7057c29658a2aa34b3fe540039e027c24435
maybewh <122131004@qq.com>
2021-06-23 20:40:57 +0800

    HashMap和双链表实现LinkedHashMap,leetcode通过

--- src/com/leetcode/top100/LRUCacheImplementLinkedHashMap_146.java
+++ src/com/leetcode/top100/LRUCacheImplementLinkedHashMap_146.java
@@ -5,6 +5,19 @@ import java.util.Map;
 
 public class LRUCacheImplementLinkedHashMap_146 {
 
+    public static void main(String[] args) {
+        LRUCacheImplementLinkedHashMap_146 lRUCache = new LRUCacheImplementLinkedHashMap_146(2);
+        lRUCache.put(1, 1); // 缓存是 {1=1}
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        System.out.println(lRUCache.get(1));   // 返回 1
+        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        System.out.println(lRUCache.get(2));    // 返回 -1 (未找到)
+        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        System.out.println(lRUCache.get(1));    // 返回 -1 (未找到)
+        System.out.println(lRUCache.get(3));   // 返回 3
+        System.out.println(lRUCache.get(4));   // 返回 4
+    }
+
     class DLinkedNode {
 
         int key;
@@ -30,11 +43,6 @@ public class LRUCacheImplementLinkedHashMap_146 {
     public LRUCacheImplementLinkedHashMap_146(int capacity) {
         size = 0;
         this.capacity = capacity;
-
-        head = new DLinkedNode();
-        tail = new DLinkedNode();
-        head.after = tail;
-        tail.before = head;
     }
 
     /**
@@ -53,21 +61,70 @@ public class LRUCacheImplementLinkedHashMap_146 {
     }
 
     public void put(int key, int value) {
-        DLinkedNode node = new DLinkedNode(key, value);
-        container.put(key, node);
-        size++;
-        addNodeToTail(node);
-        if (size > capacity) {
-            removeHeadNode(node);
-            size--;
+        DLinkedNode look = container.get(key);
+        if (look == null) {
+            DLinkedNode node = new DLinkedNode(key, value);
+            container.put(key, node);
+            size++;
+            addNodeToTail(node);
+            if (size > capacity) {
+                DLinkedNode head = removeHeadNode();
+                container.remove(head.key);
+                size--;
+            }
+        } else {
+            look.val = value;
+            moveNodeToTail(look);
         }
-    }
 
-
-    public void removeNode(DLinkedNode node) {
 
     }
 
+    private DLinkedNode removeHeadNode() {
+        DLinkedNode firstNode = head;
+        head = firstNode.after;
+        firstNode.after = null;
+        return firstNode;
+    }
 
+    private void moveNodeToTail(DLinkedNode node) {
+        if (node == tail) {
+            return;
+        }
+        DLinkedNode beforeNode = node.before;
+        DLinkedNode afterNode = node.after;
+
+        DLinkedNode lastNode = tail;
+
+        // 若是头结点
+        if (node == head) {
+            head = afterNode;
+            head.before = null;
+        } else {
+            afterNode.before = beforeNode;
+            beforeNode.after = afterNode;
+        }
+
+        node.after = null;
+        node.before = lastNode;
+
+        lastNode.after = node;
+        tail = node;
+
+    }
+
+    private void addNodeToTail(DLinkedNode node) {
+        DLinkedNode lastNode = tail;
+
+        if (tail == null && head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        lastNode.after = node;
+        node.before = lastNode;
+        tail = node;
+    }
 
 }
